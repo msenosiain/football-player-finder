e48eca23118c19b4c645d44c4b60ccceac5870df
Miguel Senosiain <msenosiain@gmail.com>
2018-05-21 19:33:15 -0300

    Load players on init

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import configureStore from "./store/configureStore";
 import App from './common/components/App';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {loadPlayers} from "./players/actions";
 
 const store = configureStore();
+
+store.dispatch(loadPlayers());
 
 render(
     <Provider store={store}>
